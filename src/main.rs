7afaa4bbf17b45efed5cd42c99a3095b18fbcd56
Alex Studer <alex@studer.dev>
2021-02-07 22:48:28 -0500

    push roots into a Vec and print out the vec

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use std::fs;
 
+#[derive(Debug)]
+struct Root {
+    filename: String,
+    path: std::path::PathBuf
+}
+
 fn main() {
     let devices = fs::read_dir("/sys/bus/usb/devices").unwrap();
+    let mut roots: Vec<Root> = Vec::new();
 
     for entry in devices {
         let device = if let Ok(d) = entry { d } else { continue; };
+
         let filename_str = device.file_name().into_string();
         let filename: std::string::String = filename_str.unwrap_or("".to_string());
         if !filename.starts_with("usb") {
             continue;
         }
-        println!("{}", filename);
+
+        let path = device.path();
+
+        roots.push(Root {
+            filename,
+            path
+        });
     }
+
+    println!("{:?}", roots);
 }
