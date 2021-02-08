3776734f6096daffdecee6d6f24563a5db6957a0
Alex Studer <alex@studer.dev>
2021-02-07 22:38:23 -0500

    only print the filenames instead of the whole path

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::fs;
 fn main() {
     let devices = fs::read_dir("/sys/bus/usb/devices").unwrap();
 
-    for device in devices {
-        println!("{}", device.unwrap().path().display())
+    for entry in devices {
+        let device = if let Ok(d) = entry { d } else { continue; };
+        println!("{:?}", device.file_name());
     }
 }
